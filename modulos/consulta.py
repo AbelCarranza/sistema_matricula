b4b788b22601b98886393f5b722792dcebc297d1
Abel Carranza <abelcc05055@gmail.com>
2026-09-17 12:35:33 -0500

    Ver cursos con prerrequisitos

--- modulos/consulta.py
+++ modulos/consulta.py
@@ -9,8 +9,6 @@ def mostrar_cursos():
         print(f"Código: {curso['codigo']}")
         print(f"Curso: {curso['nombre']}")
         print(f"Créditos: {curso['creditos']}")
-       #  print(f"Prerrequisito: {curso['prerrequisito']}")
-       #  print(f"Horario:         {curso['dia']} {curso['hora_inicio']:.2f} - {curso['hora_fin']:.2f}")
         print("-" * 40)
 
 
@@ -29,3 +27,13 @@ def buscar_curso_code(codigo):
         
 
     print("Curso no existe")
+
+def ver_curso_pre():
+    for curso in cursos:
+        if curso["prerrequisito"] != "Ninguno":
+            print("\nCursos con prerrequisitos")
+            print(f"Código: {curso['codigo']}")
+            print(f"Curso: {curso['nombre']}")
+            print(f"Créditos: {curso['creditos']}")           
+            print(f"Prerrequisito: {curso['prerrequisito']}")
+            print(f"Horario:         {curso['dia']} {curso['hora_inicio']:.2f} - {curso['hora_fin']:.2f}")
